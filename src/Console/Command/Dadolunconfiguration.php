29f56602a9d106e50bbc1e9136bf9ca94d7f3d44
davide.lunardon@hevelop.com <davide.lunardon@hevelop.com>
2019-07-11 17:44:05 +0200

    Update old namespaces

--- src/Console/Command/Dadolunconfiguration.php
+++ src/Console/Command/Dadolunconfiguration.php
@@ -6,7 +6,7 @@ use DadolunHook\App\Runner\Dadolunconfigurator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use DadolunHook\App\Console\Command\Base as CaptainHookBase;
+use CaptainHook\App\Console\Command\Base as CaptainHookBase;
 
 /**
  * Class Dadolunconfiguration
